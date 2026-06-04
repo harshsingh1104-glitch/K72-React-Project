a3c23420d9e18f56b2bdb59189ed66d79faa5681
harsh singh <harshsingh1104@gmail.com>
2026-06-04 11:22:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Agence from './Pages/Agence';
+import Projects from './Pages/Projects';
+
+const App = () => {
+  return (
+    <div className='text-white'>
+      <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/agence' element={<Agence/>} />
+      <Route path='/projects' element={<Projects/>} />
+    </Routes>
+    </div>
+  )
+}
+
+export default App;
+  
+  
+
+
---
+++ src/pages/Agence.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Agence = () => {
+    return (
+        <div>Agence</div>
+    )
+}
+  export default Agence;
